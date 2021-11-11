d881b40f7acd935fe57465684e7010ac3bd43753
pritamjadhav <88623137+pritamyj@users.noreply.github.com>
2021-11-12 02:21:37 +0530

    Update navbar.js

--- themes/custom/mytheme/js/navbar.js
+++ themes/custom/mytheme/js/navbar.js
@@ -2,14 +2,14 @@
 
  (function($){
         $(window).scroll(function () {
-            if ($(this).scrollTop() < 50){
+            if ($(this).scrollTop() < 30){
                 $("header #block-mytheme-main-menu ").css({"background-color":"transparent","background-image": "linear-gradient(326deg, transparent 0%, transparent 74%)",
-    "padding": "45px 0","margin-top": "82px"}); 
+    "padding": "45px 0","margin-top": "60px"}); 
 
                 $(".heading ").css({"margin-top":"183px","transition": "all 0.5s ease-in-out"})
                
             }else{
-                $("header #block-mytheme-main-menu ").css({"background-color":"#bd4f6c",'position': 'fixed','top': '0','width': '100%','z-index':'1',"background-image": "linear-gradient(326deg, #bd4f6c 0%, #d7816a 74%)",
+                $("header #block-mytheme-main-menu ").css({"background-color":"#bd4f6c",'position': 'fixed','top': '0','width': '100%','z-index':'9',"background-image": "linear-gradient(326deg, #bd4f6c 0%, #d7816a 74%)",
     "padding": "7px 0","margin-top": "0" });
 
     $("header .language-switcher-language-url").css({"background-color":"#bd4f6c",'top': '23px','width': '100%','z-index':'1',"background-image": "linear-gradient(326deg, #bd4f6c 0%, #d7816a 74%)" }); 
@@ -18,9 +18,7 @@
 }
 })
 
-    // $(document).ready(function(){
-    //     alert();
-    //     });
+    
     }
     (jQuery));
     
